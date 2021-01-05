a319a59bf7df353ede7cbf03a97f5f46a5727d9b
matejsgit <matej.stefanic@icloud.com>
2021-01-05 13:30:38 +0100

    Update

--- myflexgit_flexbe_states/src/myflexgit_flexbe_states/Call_joint_min_jerk_action_server.py
+++ myflexgit_flexbe_states/src/myflexgit_flexbe_states/Call_joint_min_jerk_action_server.py
@@ -8,18 +8,18 @@ import robot_module_msgs.msg
 class CallJointMinJerk(EventState):
 
     '''
-    Calls JointMinJerkAction server @ 'joint_min_jerk_action_server' topic
+    Calls JointMinJerkAction server @ '/joint_min_jerk_action_server' topic
     [...]
     
-    -- positions	string []	Position data input [float, float, ...] of 7 joints
-    -- speed		float		Speed data
-    -- timestep	    float		Timestamp data
+    -- goal_joint_pos   	string []	Position data input [float, float, ...] of 7 joints
+    -- motion_duration		float		Speed data
+    -- motion_timestep	    float		Timestamp data
     #< minjerk_out	[]  		minjerk action server reply userdata output
     <= continue                 Written successfully
     <= failed                  	Failed
     '''
 
-    def __init__(self, positions, speed, timestep):
+    def __init__(self, goal_joint_pos, motion_duration, motion_timestep):
         super(CallJointMinJerk, self).__init__(outcomes = ['continue', 'failed'], output_keys = ['minjerk_out'])
 
         # replace 'panda_2/joint_min_jerk_action_server' from dummies with 'joint_min_jerk_action_server'
@@ -31,9 +31,9 @@ class CallJointMinJerk(EventState):
         #self._client = ProxyActionClient({self._topic: robot_module_msgs.msg.JointMinJerkAction}) # pass required clients as dict (topic: type)
         # JointMinJerkAction
         # Input parameters of robot_module_msgs.msg
-        self._position = positions
-        self._speed = speed
-        self._timestep = timestep
+        self._position = goal_joint_pos
+        self._duration = motion_duration
+        self._timestep = motion_timestep
 
     def execute(self, userdata):
         result = ...
@@ -49,14 +49,13 @@ class CallJointMinJerk(EventState):
         return 'continue'
             
     def on_enter(self, userdata):
-        # input [float, float, ...] of 7 joints is used for self.goal_minjerk.
-        self.goal_minjerk = self._position
-        goal = robot_module_msgs.msg.JointMinJerkGoal(self.goal_minjerk, self._speed, self._timestep)
+        # input [float, float, ...] of 7 joints is used for self._position.
+        goal = robot_module_msgs.msg.JointMinJerkGoal(self._position, self._duration, self._timestep)
         Logger.loginfo("Starting sending goal...")
 
         try:
             self._client.send_goal_and_wait(goal, execute_timeout=rospy.Duration(5))
-            Logger.loginfo("Goal sent: {}".format(str(self.goal_minjerk)))
+            Logger.loginfo("Goal sent: {}".format(str(self._position)))
         except Exception as e:
 			# Since a state failure not necessarily causes a behavior failure, it is recommended to only print warnings, not errors.
 			# Using a linebreak before appending the error log enables the operator to collapse details in the GUI.
