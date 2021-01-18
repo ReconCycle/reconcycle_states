8a946328bd6405d09bc1676d446c74d7c94ba0f0
Your Name <you@example.com>
2021-01-18 15:48:12 +0000

    Preparing for soft hand and read state

---
+++ myflexgit_flexbe_behaviors/src/myflexgit_flexbe_behaviors/pick_and_drop_test_sm.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###########################################################
+#               WARNING: Generated code!                  #
+#              **************************                 #
+# Manual changes may get lost if file is generated again. #
+# Only code inside the [MANUAL] tags will be kept.        #
+###########################################################
+
+from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyContainer, PriorityContainer, Logger
+from myflexgit_flexbe_behaviors.flexbe_mongowrite_sm import FlexBe_mongoWriteSM
+from myflexgit_flexbe_states.Call_joint_min_jerk_action_server import CallJointMinJerk
+# Additional imports can be added inside the following tags
+# [MANUAL_IMPORT]
+
+# [/MANUAL_IMPORT]
+
+
+'''
+Created on Mon Jan 18 2021
+@author: Rok Pahic
+'''
+class Pick_and_Drop_testSM(Behavior):
+	'''
+	Testing Pick and drop in Reconcycle
+	'''
+
+
+	def __init__(self):
+		super(Pick_and_Drop_testSM, self).__init__()
+		self.name = 'Pick_and_Drop_test'
+
+		# parameters of this behavior
+
+		# references to used behaviors
+		self.add_behavior(FlexBe_mongoWriteSM, 'FlexBe_mongoWrite')
+
+		# Additional initialization code can be added inside the following tags
+		# [MANUAL_INIT]
+		
+		# [/MANUAL_INIT]
+
+		# Behavior comments:
+
+
+
+	def create(self):
+		# x:30 y:365, x:130 y:365
+		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed'])
+		_state_machine.userdata.goal_joint_pos = [1,1,1,1,1,1,1]
+		_state_machine.userdata.entry_name = 'test flexbe'
+
+		# Additional creation code can be added inside the following tags
+		# [MANUAL_CREATE]
+		
+		# [/MANUAL_CREATE]
+
+
+		with _state_machine:
+			# x:70 y:149
+			OperatableStateMachine.add('Move to start',
+										CallJointMinJerk(motion_duration=1, motion_timestep=0.1),
+										transitions={'continue': 'FlexBe_mongoWrite', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off},
+										remapping={'goal_joint_pos': 'goal_joint_pos', 'minjerk_out': 'minjerk_out'})
+
+			# x:378 y:92
+			OperatableStateMachine.add('FlexBe_mongoWrite',
+										self.use_behavior(FlexBe_mongoWriteSM, 'FlexBe_mongoWrite'),
+										transitions={'finished': 'finished', 'failed': 'failed'},
+										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit},
+										remapping={'entry_data': 'minjerk_out', 'entry_name': 'entry_name'})
+
+
+		return _state_machine
+
+
+	# Private functions can be added inside the following tags
+	# [MANUAL_FUNC]
+	
+	# [/MANUAL_FUNC]
---
+++ myflexgit_flexbe_states/src/myflexgit_flexbe_states/MoveSoftHand.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import actionlib
+import rospy
+from flexbe_core import EventState, Logger
+import robot_module_msgs.msg
+import time
+
+class CallJointMinJerk(EventState):
+
+    '''
+    Calls JointMinJerkAction server @ '/joint_min_jerk_action_server' topic
+    [...]
+    
+    -- motion_duration  float		Speed data
+    -- motion_timestep  float		Timestamp data  
+    ># goal_joint_pos   string []	Position data input [float, float, ...] of 7 joints
+    #< minjerk_out      sting []  	minjerk action server reply userdata output
+    <= continue                 Written successfully
+    <= failed                  	Failed
+    '''
+
+    def __init__(self, motion_duration, motion_timestep):
+        super(CallJointMinJerk, self).__init__(outcomes = ['continue', 'failed'], input_keys = ['goal_joint_pos'], output_keys = ['minjerk_out'])
+
+        # actionlib client @joint_min_jerk_action_server
+        self._client = actionlib.SimpleActionClient('joint_min_jerk_action_server', robot_module_msgs.msg.JointMinJerkAction)
+
+        #self._client = ProxyActionClient({self._topic: robot_module_msgs.msg.JointMinJerkAction}) # pass required clients as dict (topic: type)
+        # JointMinJerkAction
+        # Input parameters of robot_module_msgs.msg
+        self._duration = motion_duration
+        self._timestep = motion_timestep
+
+    def execute(self, userdata):
+        result = []
+
+        try:
+            result = self._client.get_result()
+            userdata.minjerk_out = result 
+            Logger.loginfo("Action Server reply: \n {}".format(str(userdata.minjerk_out)))
+        except Exception as e:
+            Logger.loginfo("No result or server is not active!")
+            return 'failed'
+
+        return 'continue'
+            
+    def on_enter(self, userdata):
+        # input [float, float, ...] of 7 joints is used for userdata.goal_joint_pos.
+        goal = robot_module_msgs.msg.JointMinJerkGoal(userdata.goal_joint_pos, self._duration, self._timestep)
+        Logger.loginfo("Starting sending goal...")
+
+        try:
+            Logger.loginfo("Goal sent: {}".format(str(userdata.goal_joint_pos)))
+            self._client.send_goal(goal)
+            while self._client.get_result() == None:
+                Logger.loginfo("{}".format(robot_module_msgs.msg.JointMinJerkFeedback()))
+                time.sleep(0.2)
+
+        except Exception as e:
+            # Since a state failure not necessarily causes a behavior failure, it is recommended to only print warnings, not errors.
+			# Using a linebreak before appending the error log enables the operator to collapse details in the GUI.
+            Logger.loginfo('Failed to send the goal command:\n{}'.format(str(e)))
+            self._error = True
+
+    def on_exit(self, userdata):
+        if not self._client.get_result():
+            self._client.cancel_goal()
+            Logger.loginfo('Cancelled active action goal. No reply data.')
+        Logger.loginfo('Finished sending goal to JointMinJerkGoal.')
+        return 'continue'
