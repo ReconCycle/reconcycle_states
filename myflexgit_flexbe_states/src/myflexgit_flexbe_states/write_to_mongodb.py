0c471553d6d6fb466d4f94d3512f45f914e32bdc
matejsgit <67898492+matejsgit@users.noreply.github.com>
2021-01-21 09:56:11 +0100

    Update

--- myflexgit_flexbe_states/src/myflexgit_flexbe_states/write_to_mongodb.py
+++ myflexgit_flexbe_states/src/myflexgit_flexbe_states/write_to_mongodb.py
@@ -42,7 +42,7 @@ class WriteToMongo(EventState):
         pos = JointState(position=entry_data)
         
         try: 
-            present_data = self.msg_store.query(str(userdata.entry_name))
+            present_data = self.msg_store.query_named(str(userdata.entry_name), JointState._type))
             Logger.loginfo("Data in DB: {}".format(present_data)) 
             if present_data != []:
                 Logger.loginfo("Id {} already exists in DB...Updating data.".format(userdata.entry_name))
