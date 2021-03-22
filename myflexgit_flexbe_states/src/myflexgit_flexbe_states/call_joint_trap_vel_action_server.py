38ce90ae0ecc6b51c48b4b19b32014041504f19f
reconcycle <reconcycle@reconcycle-dev1>
2021-03-22 07:58:00 +0000

    trapez vel in work

--- myflexgit_flexbe_states/src/myflexgit_flexbe_states/call_joint_trap_vel_action_server.py
+++ myflexgit_flexbe_states/src/myflexgit_flexbe_states/call_joint_trap_vel_action_server.py
@@ -4,9 +4,13 @@ import actionlib
 import rospy
 from flexbe_core import EventState, Logger
 from flexbe_core.proxy import ProxyActionClient
+
 from sensor_msgs.msg import JointState
-from actionlib_msgs.msg import GoalStatus
-import robot_module_msgs.msg
+
+
+from robot_module_msgs.msg import JointTrapVelAction,JointTrapVelGoal
+
+
 
 class CallJointTrap(EventState):
 
@@ -22,20 +26,22 @@ class CallJointTrap(EventState):
     <= failed                       Failed
     '''
 
-    def __init__(self):
+    def __init__(self,max_vel,max_acl,namespace=''):
         super(CallJointTrap, self).__init__(outcomes = ['continue', 'failed'], input_keys = ['joints_data'], output_keys = ['joint_values'])
 
         # replace 'panda_2/joint_trap_vel_action_server' from dummies with 'joint_trap_vel_action_server'
-        self._topic = 'panda_2/joint_trap_vel_action_server'
-        #self._topic = 'joint_trap_vel_action_server'
-        self._client = actionlib.SimpleActionClient(self._topic, robot_module_msgs.msg.JointTrapVelAction)
+        self._namespace=namespace
+        #self._topic = self._namespace + '/joint_impedance_controller/move_joint_trap'
+        self._topic =  '/joint_impedance_controller/move_joint_trap'
+
+        self._client = ProxyActionClient({ self._topic: JointTrapVelAction})
         #self._client = ProxyActionClient({self._topic: robot_module_msgs.msg.JointTrapVelAction}) # pass required clients as dict (topic: type)
         # JointTrapVelAction
 
 		# It may happen that the action client fails to send the action goal.
 
     def execute(self, userdata):
-        result = ...
+        
 
         try:
             result = self._client.get_result()
@@ -50,11 +56,15 @@ class CallJointTrap(EventState):
     def on_enter(self, userdata):
         # JointState() is used for tests. Replace JointState() with userdata.joints_data.
         self.goal_joint = userdata.joints_data #JointState()
-        goal = robot_module_msgs.msg.JointTrapVelGoal([self.goal_joint], 0.05, None)
+        joint = JointState()
+        joint.position = self.goal_joint
+        goal = JointTrapVelGoal([joint], 0.3, 0.3)
+
+        print(goal)
         Logger.loginfo("Starting sending goal...")
 
         try:
-            self._client.send_goal_and_wait(goal, execute_timeout=rospy.Duration(5))
+            self._client.send_goal(self._topic,goal)
             Logger.loginfo("Goal sent: {}".format(str(self.goal_joint)))
         except Exception as e:
 			# Since a state failure not necessarily causes a behavior failure, it is recommended to only print warnings, not errors.
@@ -63,9 +73,34 @@ class CallJointTrap(EventState):
             self._error = True
 
     def on_exit(self, userdata):
-        if not self._client.get_result():
-            self._client.cancel_goal()
+        if not self._client.get_result(self._topic):
+            self._client.cancel(self._topic)
             Logger.loginfo('Cancelled active action goal.')
         Logger.loginfo('Finished sending goal to JointTrapVelGoal.')
         return 'continue'
         
+
+
+if __name__ == '__main__':
+
+    print("Testing standalone")
+
+    class userdata():
+
+        def __init__(self,pos):
+
+            self.joints_data=pos
+
+    
+    rospy.init_node('test_node')
+    test_state=CallJointTrap(0.5,0.1)
+    usertest=userdata([1,1,1,1,1,1,1])
+    test_state.on_enter(usertest)
+    #test_state.execute(usertest)
+    #test_state.on_exit(usertest)
+
+
+   # usertest=userdata([0.1,0.1,0.1,0.1,0.1,0.1,0.1])
+    #test_state.on_enter(usertest)
+    #test_state.execute(usertest)
+    #test_state.on_exit(usertest)
