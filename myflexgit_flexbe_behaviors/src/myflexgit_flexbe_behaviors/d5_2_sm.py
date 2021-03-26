204b441fddbac6c299be43db337f94d63a934ff5
reconcycle <reconcycle@reconcycle-dev1>
2021-03-26 09:20:59 +0000

    init_prepare

--- myflexgit_flexbe_behaviors/src/myflexgit_flexbe_behaviors/d5_2_sm.py
+++ myflexgit_flexbe_behaviors/src/myflexgit_flexbe_behaviors/d5_2_sm.py
@@ -9,6 +9,8 @@
 
 from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyContainer, PriorityContainer, Logger
 from myflexgit_flexbe_states.avtivate_raspi_output import ActivateRaspiDigitalOuput
+from myflexgit_flexbe_states.call_joint_trap_vel_action_server import CallJointTrap
+from myflexgit_flexbe_states.read_from_mongodb import ReadFromMongo
 # Additional imports can be added inside the following tags
 # [MANUAL_IMPORT]
 
@@ -46,6 +48,7 @@ class D5_2SM(Behavior):
 		# x:741 y:94, x:723 y:338
 		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed'])
 		_state_machine.userdata.TR = True
+		_state_machine.userdata.panda1_init_position = 'panda_1_init'
 
 		# Additional creation code can be added inside the following tags
 		# [MANUAL_CREATE]
@@ -65,7 +68,7 @@ class D5_2SM(Behavior):
 
 
 		# x:904 y:39, x:73 y:654
-		_sm_cell_runing_1 = OperatableStateMachine(outcomes=['finished', 'failed'], input_keys=['TR'])
+		_sm_cell_runing_1 = OperatableStateMachine(outcomes=['finished', 'failed'], input_keys=['TR', 'panda1_init_position'])
 
 		with _sm_cell_runing_1:
 			# x:97 y:96
@@ -76,16 +79,30 @@ class D5_2SM(Behavior):
 										remapping={'value': 'TR', 'success': 'success'})
 
 
-		# x:440 y:42, x:531 y:232
-		_sm_cell_init_2 = OperatableStateMachine(outcomes=['finished', 'failed'], input_keys=['TR'])
+		# x:890 y:31, x:167 y:451
+		_sm_cell_init_2 = OperatableStateMachine(outcomes=['finished', 'failed'], input_keys=['TR', 'panda1_init_position'])
 
 		with _sm_cell_init_2:
-			# x:145 y:49
+			# x:22 y:82
 			OperatableStateMachine.add('Activate CLAMP air block ',
 										ActivateRaspiDigitalOuput(service_name='/obr_block2_ON'),
-										transitions={'continue': 'finished', 'failed': 'failed'},
+										transitions={'continue': 'Read initial state', 'failed': 'failed'},
 										autonomy={'continue': Autonomy.Low, 'failed': Autonomy.Low},
 										remapping={'value': 'TR', 'success': 'success'})
+
+			# x:464 y:141
+			OperatableStateMachine.add('Move Panda 1 to initial',
+										CallJointTrap(max_vel=0.3, max_acl=0.3, namespace='/panda_1'),
+										transitions={'continue': 'finished', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off},
+										remapping={'joints_data': 'init_position', 'joint_values': 'joint_values'})
+
+			# x:256 y:70
+			OperatableStateMachine.add('Read initial state',
+										ReadFromMongo(),
+										transitions={'continue': 'Move Panda 1 to initial', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Low, 'failed': Autonomy.Off},
+										remapping={'entry_name': 'panda1_init_position', 'joints_data': 'init_position'})
 
 
 
@@ -95,14 +112,14 @@ class D5_2SM(Behavior):
 										_sm_cell_init_2,
 										transitions={'finished': 'Cell runing', 'failed': 'failed'},
 										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit},
-										remapping={'TR': 'TR'})
+										remapping={'TR': 'TR', 'panda1_init_position': 'panda1_init_position'})
 
 			# x:268 y:50
 			OperatableStateMachine.add('Cell runing',
 										_sm_cell_runing_1,
 										transitions={'finished': 'Cell shotdown', 'failed': 'failed'},
 										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit},
-										remapping={'TR': 'TR'})
+										remapping={'TR': 'TR', 'panda1_init_position': 'panda1_init_position'})
 
 			# x:477 y:58
 			OperatableStateMachine.add('Cell shotdown',
