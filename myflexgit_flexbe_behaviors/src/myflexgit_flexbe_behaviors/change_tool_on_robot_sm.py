be227780be3ee37413b51f9477e3ee5e3dae62e8
reconcycle <reconcycle@reconcycle-dev1>
2021-03-27 14:25:27 +0000

    untested change tool

---
+++ myflexgit_flexbe_behaviors/src/myflexgit_flexbe_behaviors/change_tool_on_robot_sm.py
@@ -0,0 +1,261 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###########################################################
+#               WARNING: Generated code!                  #
+#              **************************                 #
+# Manual changes may get lost if file is generated again. #
+# Only code inside the [MANUAL] tags will be kept.        #
+###########################################################
+
+from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyContainer, PriorityContainer, Logger
+from myflexgit_flexbe_states.avtivate_raspi_output import ActivateRaspiDigitalOuput
+from myflexgit_flexbe_states.call_joint_trap_vel_action_server import CallJointTrap
+from myflexgit_flexbe_states.read_from_mongodb import ReadFromMongo
+# Additional imports can be added inside the following tags
+# [MANUAL_IMPORT]
+
+# [/MANUAL_IMPORT]
+
+
+'''
+Created on Fri Mar 26 2021
+@author: Rok Pahic
+'''
+class ChangetoolonrobotSM(Behavior):
+	'''
+	Pick up heat alocator from table and put in clamp
+	'''
+
+
+	def __init__(self):
+		super(ChangetoolonrobotSM, self).__init__()
+		self.name = 'Change tool on robot'
+
+		# parameters of this behavior
+		self.add_parameter('clamp_service_name', ''/obr_activate'')
+		self.add_parameter('max_acl', 0.3)
+		self.add_parameter('namespace', ''panda_2'')
+		self.add_parameter('max_vel', 0.3)
+		self.add_parameter('air_block_service_name', '/Panda2BlockON')
+		self.add_parameter('tool_unlock_service_name', '/Panda2ToolUnlock')
+
+		# references to used behaviors
+
+		# Additional initialization code can be added inside the following tags
+		# [MANUAL_INIT]
+		
+		# [/MANUAL_INIT]
+
+		# Behavior comments:
+
+
+
+	def create(self):
+		# x:84 y:259, x:723 y:338
+		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed'], input_keys=['tool_drop_location_name', 'tool_take_location_name', 'before_drop_location_name', 'after_take_location_name'])
+		_state_machine.userdata.TR = True
+		_state_machine.userdata.FA = False
+		_state_machine.userdata.tool_drop_location_name = 'panda_2_tool_change'
+		_state_machine.userdata.tool_take_location_name = 'panda_2_tool_change'
+		_state_machine.userdata.before_drop_location_name = 'panda_2_tool_change'
+		_state_machine.userdata.after_take_location_name = 'panda_2_tool_change'
+
+		# Additional creation code can be added inside the following tags
+		# [MANUAL_CREATE]
+		
+		# [/MANUAL_CREATE]
+
+		# x:30 y:365, x:130 y:365
+		_sm_move_to_take__0 = OperatableStateMachine(outcomes=['finished', 'failed'], input_keys=['position_name'])
+
+		with _sm_move_to_take__0:
+			# x:182 y:50
+			OperatableStateMachine.add('Read robot position',
+										ReadFromMongo(),
+										transitions={'continue': 'Move to robot position', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off},
+										remapping={'entry_name': 'position_name', 'joints_data': 'joints_positions'})
+
+			# x:500 y:117
+			OperatableStateMachine.add('Move to robot position',
+										CallJointTrap(max_vel=self.max_vel, max_acl=self.max_acl, namespace=self.namespace),
+										transitions={'continue': 'finished', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Low, 'failed': Autonomy.Low},
+										remapping={'joints_data': 'joints_positions', 'joint_values': 'joint_values'})
+
+
+		# x:30 y:365, x:130 y:365
+		_sm_move_to_drop_1 = OperatableStateMachine(outcomes=['finished', 'failed'], input_keys=['position_name'])
+
+		with _sm_move_to_drop_1:
+			# x:182 y:50
+			OperatableStateMachine.add('Read robot position',
+										ReadFromMongo(),
+										transitions={'continue': 'Move to robot position', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off},
+										remapping={'entry_name': 'position_name', 'joints_data': 'joints_positions'})
+
+			# x:500 y:117
+			OperatableStateMachine.add('Move to robot position',
+										CallJointTrap(max_vel=self.max_vel, max_acl=self.max_acl, namespace=self.namespace),
+										transitions={'continue': 'finished', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Low, 'failed': Autonomy.Low},
+										remapping={'joints_data': 'joints_positions', 'joint_values': 'joint_values'})
+
+
+		# x:30 y:365, x:130 y:365
+		_sm_move_over_take_location_2_2 = OperatableStateMachine(outcomes=['finished', 'failed'], input_keys=['position_name'])
+
+		with _sm_move_over_take_location_2_2:
+			# x:182 y:50
+			OperatableStateMachine.add('Read robot position',
+										ReadFromMongo(),
+										transitions={'continue': 'Move to robot position', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off},
+										remapping={'entry_name': 'position_name', 'joints_data': 'joints_positions'})
+
+			# x:500 y:117
+			OperatableStateMachine.add('Move to robot position',
+										CallJointTrap(max_vel=self.max_vel, max_acl=self.max_acl, namespace=self.namespace),
+										transitions={'continue': 'finished', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Low, 'failed': Autonomy.Low},
+										remapping={'joints_data': 'joints_positions', 'joint_values': 'joint_values'})
+
+
+		# x:30 y:365, x:130 y:365
+		_sm_move_over_take_location_3 = OperatableStateMachine(outcomes=['finished', 'failed'], input_keys=['position_name'])
+
+		with _sm_move_over_take_location_3:
+			# x:182 y:50
+			OperatableStateMachine.add('Read robot position',
+										ReadFromMongo(),
+										transitions={'continue': 'Move to robot position', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off},
+										remapping={'entry_name': 'position_name', 'joints_data': 'joints_positions'})
+
+			# x:500 y:117
+			OperatableStateMachine.add('Move to robot position',
+										CallJointTrap(max_vel=self.max_vel, max_acl=self.max_acl, namespace=self.namespace),
+										transitions={'continue': 'finished', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Low, 'failed': Autonomy.Low},
+										remapping={'joints_data': 'joints_positions', 'joint_values': 'joint_values'})
+
+
+		# x:30 y:365, x:130 y:365
+		_sm_move_over_drop_location_2_4 = OperatableStateMachine(outcomes=['finished', 'failed'], input_keys=['position_name'])
+
+		with _sm_move_over_drop_location_2_4:
+			# x:182 y:50
+			OperatableStateMachine.add('Read robot position',
+										ReadFromMongo(),
+										transitions={'continue': 'Move to robot position', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off},
+										remapping={'entry_name': 'position_name', 'joints_data': 'joints_positions'})
+
+			# x:500 y:117
+			OperatableStateMachine.add('Move to robot position',
+										CallJointTrap(max_vel=self.max_vel, max_acl=self.max_acl, namespace=self.namespace),
+										transitions={'continue': 'finished', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Low, 'failed': Autonomy.Low},
+										remapping={'joints_data': 'joints_positions', 'joint_values': 'joint_values'})
+
+
+		# x:30 y:365, x:130 y:365
+		_sm_move_over_drop_location_5 = OperatableStateMachine(outcomes=['finished', 'failed'], input_keys=['position_name'])
+
+		with _sm_move_over_drop_location_5:
+			# x:182 y:50
+			OperatableStateMachine.add('Read robot position',
+										ReadFromMongo(),
+										transitions={'continue': 'Move to robot position', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off},
+										remapping={'entry_name': 'position_name', 'joints_data': 'joints_positions'})
+
+			# x:500 y:117
+			OperatableStateMachine.add('Move to robot position',
+										CallJointTrap(max_vel=self.max_vel, max_acl=self.max_acl, namespace=self.namespace),
+										transitions={'continue': 'finished', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Low, 'failed': Autonomy.Low},
+										remapping={'joints_data': 'joints_positions', 'joint_values': 'joint_values'})
+
+
+
+		with _state_machine:
+			# x:130 y:38
+			OperatableStateMachine.add('Move over drop location',
+										_sm_move_over_drop_location_5,
+										transitions={'finished': 'Move to drop', 'failed': 'failed'},
+										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit},
+										remapping={'position_name': 'before_drop_location_name'})
+
+			# x:533 y:546
+			OperatableStateMachine.add('Lock tool',
+										ActivateRaspiDigitalOuput(service_name=self.tool_unlock_service_name),
+										transitions={'continue': 'Open air block', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Low, 'failed': Autonomy.Low},
+										remapping={'value': 'FA', 'success': 'success'})
+
+			# x:987 y:256
+			OperatableStateMachine.add('Move over drop location_2',
+										_sm_move_over_drop_location_2_4,
+										transitions={'finished': 'Move over take location', 'failed': 'failed'},
+										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit},
+										remapping={'position_name': 'before_drop_location_name'})
+
+			# x:934 y:561
+			OperatableStateMachine.add('Move over take location',
+										_sm_move_over_take_location_3,
+										transitions={'finished': 'Move to take ', 'failed': 'failed'},
+										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit},
+										remapping={'position_name': 'after_take_location_name'})
+
+			# x:68 y:530
+			OperatableStateMachine.add('Move over take location_2',
+										_sm_move_over_take_location_2_2,
+										transitions={'finished': 'finished', 'failed': 'failed'},
+										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit},
+										remapping={'position_name': 'after_take_location_name'})
+
+			# x:436 y:36
+			OperatableStateMachine.add('Move to drop',
+										_sm_move_to_drop_1,
+										transitions={'finished': 'Close air block', 'failed': 'failed'},
+										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit},
+										remapping={'position_name': 'tool_drop_location_name'})
+
+			# x:711 y:549
+			OperatableStateMachine.add('Move to take ',
+										_sm_move_to_take__0,
+										transitions={'finished': 'Lock tool', 'failed': 'failed'},
+										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit},
+										remapping={'position_name': 'tool_take_location_name'})
+
+			# x:322 y:553
+			OperatableStateMachine.add('Open air block',
+										ActivateRaspiDigitalOuput(service_name=self.air_block_service_name),
+										transitions={'continue': 'Move over take location_2', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Low, 'failed': Autonomy.Low},
+										remapping={'value': 'TR', 'success': 'success'})
+
+			# x:900 y:44
+			OperatableStateMachine.add('Unlock tool',
+										ActivateRaspiDigitalOuput(service_name=self.tool_unlock_service_name),
+										transitions={'continue': 'Move over drop location_2', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Low, 'failed': Autonomy.Low},
+										remapping={'value': 'TR', 'success': 'success'})
+
+			# x:676 y:42
+			OperatableStateMachine.add('Close air block',
+										ActivateRaspiDigitalOuput(service_name=self.air_block_service_name),
+										transitions={'continue': 'Unlock tool', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Low, 'failed': Autonomy.Low},
+										remapping={'value': 'FA', 'success': 'success'})
+
+
+		return _state_machine
+
+
+	# Private functions can be added inside the following tags
+	# [MANUAL_FUNC]
+	
+	# [/MANUAL_FUNC]
